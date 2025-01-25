64c1087cd50b668456e9fa33038e5cf2f5eab2df
Filipe Vilas Boas <fvb.boas@gmail.com>
2025-01-25 12:11:21 -0300

    test: testing the website update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="container-home">
+      <div className="row">
+        <button
+          onClick={() => {
+            setCount(count + 10);
+          }}
+        >
+          ⬆️
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="row middle">
+        <button
+          onClick={() => {
+            setCount(count - 1);
+          }}
+        >
+          ⬅️
+        </button>
+        <span>{count}</span>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          ➡️
+        </button>
+      </div>
+      <div className="row">
+        <button
+          onClick={() => {
+            setCount(count - 10);
+          }}
+        >
+          ⬇️
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
